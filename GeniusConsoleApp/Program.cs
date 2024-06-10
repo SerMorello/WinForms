62c733f71d6a41d9df1c176d498f6ed59eafcca4
SerMorello <morellosharp@gmail.com>
2024-06-10 11:37:49 +0300

    создал класс Question

--- GeniusConsoleApp/Program.cs
+++ GeniusConsoleApp/Program.cs
@@ -2,7 +2,7 @@
 string user = Console.ReadLine();
 int countQuestions = 5;
 List<int> numberQuestions = new List<int>();
-string[] questions = GetQuestions(countQuestions);
+string[] questions = Questions.GetQuestions(countQuestions);
 int[] answers = new int[countQuestions];
 answers[0] = 6;
 answers[1] = 9;
@@ -66,15 +66,4 @@ while (endGame == false)
     {
         endGame = true;
     }
-}
-
-static string[] GetQuestions(int countQuestions)
-{
-    string[] questions = new string[countQuestions];
-    questions[0] = "Сколько будет два плюс два умноженное на два?";
-    questions[1] = "Бревно нужно распилить на 10 частей. Сколько распилов нужно сделать?";
-    questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
-    questions[3] = "Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?";
-    questions[4] = "Пять свечей горело, две потухли. Сколько свечей осталось?";
-    return questions;
 }
---
+++ GeniusConsoleApp/Questions.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+
+
+public class Questions
+{
+    public static string[] GetQuestions(int countQuestions)
+    {
+        string[] questions = new string[countQuestions];
+        questions[0] = "Сколько будет два плюс два умноженное на два?";
+        questions[1] = "Бревно нужно распилить на 10 частей. Сколько распилов нужно сделать?";
+        questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
+        questions[3] = "Укол делают каждые полчаса. Сколько нужно минут, чтобы сделать три укола?";
+        questions[4] = "Пять свечей горело, две потухли. Сколько свечей осталось?";
+        return questions;
+    }
+}
