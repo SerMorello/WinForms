926370ef792c12dc140dd002951b0829c6e56c8c
SerMorello <morellosharp@gmail.com>
2024-06-14 23:08:54 +0300

    Создал класс для работы с файлами и методы сериализации/десериализации Users

---
+++ GeniusConsoleApp/FileWork.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+public static class FileWork
+{
+    public static void Serialize<T>(T obj, string filePath)
+    {
+        var serializer = new JsonSerializer();
+
+        using (var sw = new StreamWriter(filePath))
+        using (JsonWriter writer = new JsonTextWriter(sw))
+        {
+            serializer.Serialize(writer, obj);
+        }
+    }
+
+    public static T Deserialize<T>(string path)
+    {
+        var serializer = new JsonSerializer();
+
+        using (var sw = new StreamReader(path))
+        using (var reader = new JsonTextReader(sw))
+        {
+            T obj = default(T);
+            
+            return (T)serializer.Deserialize(reader);
+        }
+    }
+}
--- GeniusConsoleApp/UsersResultStorage.cs
+++ GeniusConsoleApp/UsersResultStorage.cs
@@ -1,11 +1,22 @@
 ﻿public class UsersResultStorage
 {
     public List<User> Users { get; set; } = new List<User>();
+    private string path = "User.txt";
     public void GetUsersScore ()
     {
+        Users = FileWork.Deserialize<List<User>>(path);
+
         foreach (var user in Users)
         {
             Console.WriteLine($"Пользователь {user.Name} - правильных ответов {user.ShowUserScore}");
         }
+    }
+    public void AddUser(User user)
+    {
+        Users.Add(user);
+    }
+    public void SaveUsers ()
+    {
+        FileWork.Serialize<List<User>>(Users, path);
     }
 }
